685d3bda6ca0477ce6aa7ade8e988de04d108489
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2022-01-04 15:32:49 -0800

    makes array for divs(buttons)

--- js/main.js
+++ js/main.js
@@ -1,28 +1,46 @@
 /*----- constants -----*/
-const lightChoices = [
-    {color: 'blue', opacity: 1, time: 1000},
-    {color: 'red', opacity: 1, time: 1000},
-    {color: 'yellow', opacity: 1, time: 1000},
-    {color: 'green', opacity: 1, time: 1000},
-    {color: 'purple', opacity: 1, time: 1000},
-    {color: 'orange', opacity: 1, time: 1000},
-    {color: 'cyan', opacity: 1, time: 1000},
-    {color: 'dark green', opacity: 1, time: 1000}
-]
+// const lightChoices = [
+//     {color: 'blue', opacity: 1, time: 1000},
+//     {color: 'red', opacity: 1, time: 1000},
+//     {color: 'yellow', opacity: 1, time: 1000},
+//     {color: 'green', opacity: 1, time: 1000},
+//     {color: 'purple', opacity: 1, time: 1000},
+//     {color: 'orange', opacity: 1, time: 1000},
+//     {color: 'cyan', opacity: 1, time: 1000},
+//     {color: 'dark green', opacity: 1, time: 1000}
+// ]
 
 
 /*----- app's state (variables) -----*/
 
-let lights;
-let turn;
-let result;
-let goal;
+let playerSequence; // holds player moves
+let computerSequence; // holds computer moves
+let play; // true if pc turn
+let result; // false when player loose
+let gameInPlay; // true when game begins
 
 
 
 /*----- cached element references -----*/
-const lightEls = document.querySelectorAll('section > button');
+const lightEls = Array.from(document.querySelectorAll('section > button'));
 
+const startButton = document.querySelector('.replay')
+
+
+
+const lightUp = function() {
+    lightEls[0].classList.add('light')
+    lightEls[1].classList.add('light')
+    lightEls[2].classList.add('light')
+    lightEls[3].classList.add('light')
+}
+
+const standard = function() {
+    lightEls[0].classList.remove('light')
+    lightEls[1].classList.remove('light')
+    lightEls[2].classList.remove('light')
+    lightEls[3].classList.remove('light')
+}
 
 
 /*----- event listeners -----*/
@@ -36,27 +54,40 @@ document.querySelector('button')
 init();
 
 function init() {
-    lights = 0;
-    turn = 1;
-    result = 0;
-    goal = '';
-    render();
+    playerSequence =  0;
+    computerSequence =  0;
+    play =  true;
+    result =  false;
+    gameInPlay =  true;
+    // render();
 };
 
-function render() {
-    lightEls.forEach(function(color) {
-        color.style.opacity = .5;
-        lightEls[lights].style.opacity = lightChoices[lights].opacity;
-        
-    })
+// function render(cb) {
+//     isPlaying = true;
+//     let idx = 0;
+//     const timerId = setInterval(function() {
+//       const btn = buttons[idx];
+//       btn.style.opacity = 1;
+//       setTimeout(function() {
+//         btn.style.opacity = 0.5;
+//       }, LIT_TIME);
+//       idx++;
+//       if (idx === sequence.length) {
+//         clearInterval(timerId);
+//         isPlaying = false;
+//         setTimeout(cb, LIT_TIME);
+//       }
+//     }, LIT_TIME + GAP_TIME);
+//   }
 
-}
 
 function handleClick(evt) {
     if (evt.target.tagName !== 'BUTTON') return;
+    itemClick = lightEls.target;
+    
+
 
 }
-
 
 
 
