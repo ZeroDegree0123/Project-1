df5c44b92f5ea0ae0586c42e4313a39a97ed439e
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2022-01-06 14:08:33 -0800

    fixes sound bug

--- js/main.js
+++ js/main.js
@@ -45,8 +45,8 @@ const soundClips = [
     new Audio ("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
     new Audio ("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
     new Audio ("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"),
-    new Audio ("/Users/zacharyvasquez/code/Project1/sounds/square-sound-free-download-scloudtomp3downloader.com.mp3"),
-    new Audio ("/Users/zacharyvasquez/code/Project1/sounds/evil-laugh2-scloudtomp3downloader.com.mp3")
+    new Audio ("./sounds/square-sound-free-download-scloudtomp3downloader.com.mp3"),
+    new Audio ("./sounds/evil-laugh2-scloudtomp3downloader.com.mp3")
 ];
 /*----- event listeners -----*/
 
